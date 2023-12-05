5d32fb285274d2aab4424085d075fb8763beb6e4
Martin Ahlström <martin.ahlstrom@outlook.com>
2023-12-05 23:26:57 +0100

    wip

--- Puzzles/2023/Year2023Day05.cs
+++ Puzzles/2023/Year2023Day05.cs
@@ -3,85 +3,154 @@ using System.Text.RegularExpressions;
 
 namespace AdventOfCode.Puzzles
 {
-   [Puzzle(2023, 5, "If You Give A Seed A Fertilizer")]
-   public partial class Year2023Day05 : IPuzzle
-   {
-      public object FirstPart()
-      {
-         IEnumerator<string> lineEnumerator = InputReader.ReadLines(this).GetEnumerator();
+    [Puzzle(2023, 5, "If You Give A Seed A Fertilizer")]
+    public partial class Year2023Day05 : IPuzzle
+    {
+        public object FirstPart()
+        {
+            IEnumerator<string> lineEnumerator = InputReader.ReadLines(this).GetEnumerator();
 
-         lineEnumerator.MoveNext();
-         var seeds = ReadSeeds(lineEnumerator.Current);
+            lineEnumerator.MoveNext();
+            var seeds = ReadSeeds(lineEnumerator.Current);
 
-         List<Map> maps = [];
+            lineEnumerator.MoveNext();
+            Dictionary<string, Map> maps = ReadMaps(lineEnumerator).ToDictionary(x => x.From, x => x);
 
-         lineEnumerator.MoveNext();
-         while (lineEnumerator.MoveNext())
-         {
-            maps.Add(ReadMap(lineEnumerator));
-         }
+            return Calc(seeds, maps);
+        }
 
+        public object SecondPart()
+        {
+            IEnumerator<string> lineEnumerator = InputReader.ReadLines(this).GetEnumerator();
 
-         throw new NotImplementedException();
-      }
+            lineEnumerator.MoveNext();
+            var seeds = ReadSeeds(lineEnumerator.Current);
 
-      public object SecondPart()
-      {
-         throw new NotImplementedException();
-      }
+            lineEnumerator.MoveNext();
+            Dictionary<string, Map> maps = ReadMaps(lineEnumerator).ToDictionary(x => x.From, x => x);
 
-      private static List<int> ReadSSV(ReadOnlySpan<char> span, int expectedCount = 0)
-      {
-         List<int> seeds = new(expectedCount);
+            return Calc(GetSeeds(seeds), maps);
 
-         int a = 0;
-         int b = 0;
-         while (b < span.Length)
-         {
-            while (b < span.Length && span[b] != ' ')
+            IEnumerable<long> GetSeeds(List<long> seedsInput)
             {
-               b++;
+                var enumerator = seedsInput.GetEnumerator();
+                while (enumerator.MoveNext())
+                {
+                    long n = enumerator.Current;
+                    enumerator.MoveNext();
+                    var count = enumerator.Current;
+
+                    while (count > 0)
+                    {
+                        yield return n;
+                        n++;
+                        count--;
+                    }
+                }
+            }
+        }
+
+        private static object Calc(IEnumerable<long> input, Dictionary<string, Map> maps)
+        {
+            List<long> numbers = input.ToList();
+
+            Console.WriteLine("Start process {0} seeds", numbers.Count);
+
+            var buffer = new List<long>(numbers.Count);
+            string step = "seed";
+            while (step != "location")
+            {
+                Console.WriteLine(step);
+
+                var map = maps[step];
+                buffer.AddRange(numbers.Select(map.GetNumber));
+
+                numbers.Clear();
+                numbers.AddRange(buffer);
+                buffer.Clear();
+
+                step = map.To;
             }
 
-            seeds.Add(int.Parse(span[a..b]));
-            a = b + 1;
-            b = a;
-         }
+            return numbers.Min();
+        }
 
-         return seeds;
-      }
+        [GeneratedRegex("^(?<from>.+)-to-(?<to>.+) map:$")]
+        private static partial Regex MapHeaderRx();
 
-      private Map ReadMap(IEnumerator<string> lineEnumerator)
-      {
-         var rx = MapHeaderRx().Match(lineEnumerator.Current);
+        private static List<long> ReadSeeds(string line)
+        {
+            const string startText = "seeds: ";
+            if (!line.StartsWith(startText)) { throw new InvalidOperationException(); }
 
-         var from = rx.Groups["from"].Value;
-         var to = rx.Groups["to"].Value;
+            ReadOnlySpan<char> span = line.AsSpan();
 
-         var data = new List<MapData>();
-         while(lineEnumerator.MoveNext() && lineEnumerator.Current.Length > 0)
-         {
-            var ssv = ReadSSV(lineEnumerator.Current.AsSpan(), 3);
-            data.Add(new MapData(ssv[0], ssv[1], ssv[2]));
-         }
+            return ReadSSV(span[startText.Length..], 20);
+        }
 
-         return new Map(from, to, data);
-      }
+        private static List<long> ReadSSV(ReadOnlySpan<char> span, int expectedCount = 0)
+        {
+            List<long> seeds = new(expectedCount);
 
-      private static List<int> ReadSeeds(string line)
-      {
-         const string startText = "seeds: ";
-         if (!line.StartsWith(startText)) { throw new InvalidOperationException(); }
+            int a = 0;
+            int b = 0;
+            while (b < span.Length)
+            {
+                while (b < span.Length && span[b] != ' ')
+                {
+                    b++;
+                }
 
-         ReadOnlySpan<char> span = line.AsSpan();
+                seeds.Add(long.Parse(span[a..b]));
+                a = b + 1;
+                b = a;
+            }
 
-         return ReadSSV(span[startText.Length..], 20);
-      }
+            return seeds;
+        }
 
-      private record struct Map(string From, string To, IReadOnlyCollection<MapData> MapData);
-      private record struct MapData(int Source, int Destination, int Lenght);
+        private Map ReadMap(IEnumerator<string> lineEnumerator)
+        {
+            var rx = MapHeaderRx().Match(lineEnumerator.Current);
+            var map = new Map(rx.Groups["from"].Value, rx.Groups["to"].Value);
 
-      [GeneratedRegex("^(?<from>.+)-to-(?<to>.+) map:$")]
-      private static partial Regex MapHeaderRx();
-   }
+            while (lineEnumerator.MoveNext() && lineEnumerator.Current.Length > 0)
+            {
+                var ssv = ReadSSV(lineEnumerator.Current.AsSpan(), 3);
+                map.MapData.Add(new MapData(ssv[1], ssv[0], ssv[2]));
+            }
+
+            return map;
+        }
+
+        private IEnumerable<Map> ReadMaps(IEnumerator<string> lineEnumerator)
+        {
+            while (lineEnumerator.MoveNext())
+            {
+                yield return ReadMap(lineEnumerator);
+            }
+        }
+
+        private class Map(string from, string to)
+        {
+            public string From { get; } = from;
+            public IList<MapData> MapData { get; } = new List<MapData>();
+            public string To { get; } = to;
+
+            public long GetNumber(long number)
+            {
+                foreach (var data in MapData)
+                {
+                    if (number >= data.Source && number <= data.Source + data.Lenght)
+                    {
+                        number += (data.Destination - data.Source);
+                        return number;
+                    }
+                }
+                return number;
+            }
+        }
+
+        private record struct MapData(long Source, long Destination, long Lenght);
+    }
 }
